8deb9826daaf1b1d1baee8a28e3722bb217fe312
KarasTomasz <t.karas@onet.pl>
2022-04-24 22:41:16 +0200

    add implementation

--- src/main/java/pl/tkaras/api/documents/Question.java
+++ src/main/java/pl/tkaras/api/documents/Question.java
@@ -1,4 +1,25 @@
 package pl.tkaras.api.documents;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@Document
 public class Question {
+
+    @Id
+    private String id;
+    private Category category;
+    private String content;
+    private List<String> answers;
+    private Integer correctAnswer;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
