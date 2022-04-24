b78db23e55ad667b7f9f49d185ad132fe2a46253
KarasTomasz <t.karas@onet.pl>
2022-04-24 22:35:50 +0200

    remove gender

--- src/main/java/pl/tkaras/api/dto/AppUserDTO.java
+++ src/main/java/pl/tkaras/api/dto/AppUserDTO.java
@@ -4,21 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.Indexed;
-import pl.tkaras.api.documents.Gender;
 import pl.tkaras.api.documents.Score;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppUserDto {
+public class AppUserDTO {
 
     private String id;
     private String firstName;
     private String lastName;
     private String email;
-    private Gender gender;
     private Score score;
 
 }
