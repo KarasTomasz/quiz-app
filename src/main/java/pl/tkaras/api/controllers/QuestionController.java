ab10202d94bec9f06fe7fe8333ba873a8a354079
KarasTomasz <t.karas@onet.pl>
2022-04-24 22:29:02 +0200

    add QuestionController

---
+++ src/main/java/pl/tkaras/api/controllers/QuestionController.java
@@ -0,0 +1,53 @@
+package pl.tkaras.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.tkaras.api.documents.Category;
+import pl.tkaras.api.dto.QuestionDTO;
+import pl.tkaras.services.impl.QuestionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/question")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping("/category/all")
+    public ResponseEntity<List<QuestionDTO>> fetchAllQuestions(@RequestParam Category category){
+        return new ResponseEntity<>(questionService.getQuestionsByCategory(category), HttpStatus.OK);
+    }
+
+    @GetMapping("/category/")
+    public ResponseEntity<List<QuestionDTO>> fetchQuestions(@RequestParam Integer amount, @RequestParam Category category){
+        return new ResponseEntity<>(questionService.getQuestionsByCategory(amount, category), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<QuestionDTO> fetchRandomQuestion(@RequestParam Category category){
+        return new ResponseEntity<>(questionService.getRandomQuestion(category), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<QuestionDTO> addQuestion(@RequestBody QuestionDTO questionDTO){
+        return new ResponseEntity<>(questionService.addQuestion(questionDTO), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<QuestionDTO> updateQuestion(@RequestParam String id, @RequestBody QuestionDTO questionDTO){
+        return new ResponseEntity<>(questionService.updateQuestion(id, questionDTO), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<QuestionDTO> deleteQuestion(@RequestParam String id){
+        questionService.deleteQuestion(id);
+        return ResponseEntity.ok().build();
+    }
+}
