0f8a0a29699f71765a90dbe082118112d37fae97
KarasTomasz <t.karas@onet.pl>
2022-04-25 23:35:52 +0200

    remove runner

--- src/main/java/pl/tkaras/QuizAppApplication.java
+++ src/main/java/pl/tkaras/QuizAppApplication.java
@@ -1,11 +1,7 @@
 package pl.tkaras;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.tkaras.api.documents.AppUser;
-import pl.tkaras.respositories.AppUserRepository;
 
 @SpringBootApplication
 public class QuizAppApplication {
@@ -13,30 +9,4 @@ public class QuizAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizAppApplication.class, args);
 	}
-
-
-	@Bean
-	CommandLineRunner runner(AppUserRepository repository){
-		return args -> {
-			AppUser appUser = AppUser.builder()
-					.firstName("Jan")
-					.lastName("Kowalski")
-					.email("j.kowalski@o2.pl")
-					.build();
-
-			if(!repository.findByEmail(appUser.getEmail()).isPresent()){
-				repository.insert(appUser);
-				System.out.println("added");
-			}
-			else {
-				System.out.println("already exist");
-			}
-
-		};
-
-	}
-
-
-
-
 }
