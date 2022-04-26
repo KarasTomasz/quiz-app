69951660fa16b3ed84febcf7dcda2f2b5dc05ca5
KarasTomasz <t.karas@onet.pl>
2022-04-26 22:32:39 +0200

    add exception class

---
+++ src/main/java/pl/tkaras/exceptions/impl/AppUserNotFound.java
@@ -0,0 +1,18 @@
+package pl.tkaras.exceptions.impl;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import pl.tkaras.api.documents.AppUser;
+import pl.tkaras.exceptions.NotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class AppUserNotFound extends NotFoundException {
+
+    public AppUserNotFound(AppUser appUser) {
+        super(appUser);
+    }
+
+    public AppUserNotFound(String email) {
+        super(email);
+    }
+}
