0a42d925621a7cde5187ff6ab518762242dd3bb2
KarasTomasz <t.karas@onet.pl>
2022-04-26 22:36:04 +0200

    add mapToDocument, mapToDocuments

--- src/main/java/pl/tkaras/api/mappers/IMapper.java
+++ src/main/java/pl/tkaras/api/mappers/IMapper.java
@@ -6,6 +6,10 @@ public interface IMapper<T, DTO>{
 
     DTO mapToDto(T document);
 
+    T mapToDocument(DTO dto);
+
     List<DTO> mapToDtos(List<T> list);
+
+    List<T> mapToDocuments(List<DTO> list);
 
 }
