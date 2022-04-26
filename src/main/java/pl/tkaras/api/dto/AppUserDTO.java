a081283d66b1ac87c66eb03f6670535a6f7e0add
KarasTomasz <t.karas@onet.pl>
2022-04-26 22:31:07 +0200

    add password

--- src/main/java/pl/tkaras/api/dto/AppUserDTO.java
+++ src/main/java/pl/tkaras/api/dto/AppUserDTO.java
@@ -1,10 +1,10 @@
 package pl.tkaras.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pl.tkaras.api.documents.Score;
 
 @Data
 @Builder
@@ -13,9 +13,14 @@ import pl.tkaras.api.documents.Score;
 public class AppUserDTO {
 
     private String id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
-    private Score score;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
 }
