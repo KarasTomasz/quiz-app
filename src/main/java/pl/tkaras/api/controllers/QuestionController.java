786486050e08ff6e98f16fa070b886996b16aa13
KarasTomasz <t.karas@onet.pl>
2022-04-26 23:26:45 +0200

    add QuestionMapper, refactor all methods

--- src/main/java/pl/tkaras/api/controllers/QuestionController.java
+++ src/main/java/pl/tkaras/api/controllers/QuestionController.java
@@ -1,47 +1,56 @@
 package pl.tkaras.api.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.tkaras.api.documents.Category;
+import pl.tkaras.api.documents.Question;
 import pl.tkaras.api.dto.QuestionDTO;
-import pl.tkaras.services.impl.QuestionService;
+import pl.tkaras.api.mappers.impl.QuestionMapper;
+import pl.tkaras.api.services.impl.QuestionService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/question")
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final QuestionMapper questionMapper;
 
-    public QuestionController(QuestionService questionService) {
-        this.questionService = questionService;
-    }
 
     @GetMapping("/category/all")
     public ResponseEntity<List<QuestionDTO>> fetchAllQuestions(@RequestParam("category") Category category){
-        return new ResponseEntity<>(questionService.getQuestionsByCategory(category), HttpStatus.OK);
+        List<Question> questions = questionService.getQuestionsByCategory(category);
+        return new ResponseEntity<>(questionMapper.mapToDtos(questions), HttpStatus.OK);
     }
 
-    @GetMapping("/category/")
-    public ResponseEntity<List<QuestionDTO>> fetchQuestions(@RequestParam("number") Integer number, @RequestParam Category category){
-        return new ResponseEntity<>(questionService.getQuestionsByCategory(number, category), HttpStatus.OK);
+    @GetMapping("/category/random")
+    public ResponseEntity<List<QuestionDTO>> fetchRandomQuestions(@RequestParam("number") Integer number, @RequestParam Category category){
+        List<Question> questions = questionService.getQuestionsByCategory(number, category);
+        return new ResponseEntity<>(questionMapper.mapToDtos(questions), HttpStatus.OK);
     }
 
-    @GetMapping("")
-    public ResponseEntity<QuestionDTO> fetchRandomQuestion(@RequestParam("category") Category category){
-        return new ResponseEntity<>(questionService.getRandomQuestion(category), HttpStatus.OK);
+    @PostMapping("/answer")
+    public ResponseEntity<Boolean> checkAnswer(@RequestParam("questionId") String id, @RequestParam("answer") Integer answer){
+        boolean result = questionService.checkAnswer(id, answer);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping("")
     public ResponseEntity<QuestionDTO> addQuestion(@RequestBody QuestionDTO questionDTO){
-        return new ResponseEntity<>(questionService.addQuestion(questionDTO), HttpStatus.OK);
+        Question question = questionMapper.mapToDocument(questionDTO);
+        Question returnedQuestion = questionService.addQuestion(question);
+        return new ResponseEntity<>(questionMapper.mapToDto(returnedQuestion), HttpStatus.OK);
     }
 
     @PutMapping("")
     public ResponseEntity<QuestionDTO> updateQuestion(@RequestParam("id") String id, @RequestBody QuestionDTO questionDTO){
-        return new ResponseEntity<>(questionService.updateQuestion(id, questionDTO), HttpStatus.OK);
+        Question question = questionMapper.mapToDocument(questionDTO);
+        Question returnedQuestion =questionService.updateQuestion(id, question);
+        return new ResponseEntity<>(questionMapper.mapToDto(returnedQuestion), HttpStatus.OK);
 
     }
 
