ef0ddc66e4f9d45202bc42dee2d6db6cbd879ada
KarasTomasz <t.karas@onet.pl>
2022-04-27 14:16:15 +0200

    add createApiInfo

--- src/main/java/pl/tkaras/configs/SwaggerConfig.java
+++ src/main/java/pl/tkaras/configs/SwaggerConfig.java
@@ -3,9 +3,13 @@ package pl.tkaras.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @EnableSwagger2
 @Configuration
@@ -16,6 +20,19 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
-                .build();
+                .build()
+                .apiInfo(createApiInfo());
     }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("QuizApp Api",
+                "",
+                "1.0",
+                "",
+                new Contact("TKaras", "https://github.com/KarasTomasz", "t.karas@onet.pl"),
+                "",
+                "",
+                Collections.emptyList());
+    }
+
 }
