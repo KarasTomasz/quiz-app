c0aadd9d61c3562bc79b21f4ca895d173a76da0c
KarasTomasz <t.karas@onet.pl>
2022-04-28 02:18:44 +0200

    add configuration class for testContainers

---
+++ src/test/java/pl/tkaras/config/BaseIntegrationTest.java
@@ -0,0 +1,22 @@
+package pl.tkaras.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class BaseIntegrationTest {
+
+    @Container
+    private static final MongoDBContainer mongoDBContainer =
+            new MongoDBContainer("mongo:4.4.2");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}
