34235e16178200fb437be304fe205b081ca01852
KarasTomasz <t.karas@onet.pl>
2022-04-28 16:48:05 +0200

    refactor class

--- src/test/java/pl/tkaras/config/BaseIntegrationTest.java
+++ src/test/java/pl/tkaras/config/BaseIntegrationTest.java
@@ -4,19 +4,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers
-public class BaseIntegrationTest {
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-    @Container
-    private static final MongoDBContainer mongoDBContainer =
-            new MongoDBContainer("mongo:4.4.2");
+@Testcontainers
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public abstract class BaseIntegrationTest {
+
+    private static final MongoDBContainer mongoDBContainer;
+
+    static {
+        mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+        mongoDBContainer.start();
+    }
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry){
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
+
 }
