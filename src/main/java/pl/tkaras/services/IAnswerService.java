243bd4b1471d882d0b6795e4402a01adf06944e7
KarasTomasz <t.karas@onet.pl>
2022-04-28 02:12:51 +0200

    remove Answer

---
+++ src/main/java/pl/tkaras/services/IAnswerService.java
@@ -0,0 +1,16 @@
+package pl.tkaras.services;
+
+import pl.tkaras.models.documents.Answer;
+
+public interface IAnswerService {
+
+    Answer getAnswer(String id);
+
+    Answer addAnswer(Answer answer);
+
+    boolean checkAnswer(String questionId, Integer num);
+
+    Answer updateAnswer(String id, Answer answerDTO);
+
+    void deleteAnswer(String id);
+}
