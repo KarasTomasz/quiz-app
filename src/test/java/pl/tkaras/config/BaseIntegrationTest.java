08370930dff9a05ebaa0bf49a0a25c4bb9a7954c
KarasTomasz <t.karas@onet.pl>
2022-04-28 21:53:14 +0200

    add Network

--- src/test/java/pl/tkaras/config/BaseIntegrationTest.java
+++ src/test/java/pl/tkaras/config/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -13,10 +14,12 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public abstract class BaseIntegrationTest {
 
     private static final MongoDBContainer mongoDBContainer;
+    private static final Network network;
 
     static {
-        mongoDBContainer = new MongoDBContainer("mongo:4.4.2")
-                .withExposedPorts(27017);
+        network = Network.newNetwork();
+        mongoDBContainer = new MongoDBContainer("mongo:4.2")
+                .withNetwork(network);
 
         mongoDBContainer.start();
     }
