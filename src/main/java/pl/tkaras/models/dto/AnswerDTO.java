3090615b698b99ff2842387d765109dbfe68535a
KarasTomasz <t.karas@onet.pl>
2022-04-29 19:13:36 +0200

    add JsonProperty annotation

--- src/main/java/pl/tkaras/models/dto/AnswerDTO.java
+++ src/main/java/pl/tkaras/models/dto/AnswerDTO.java
@@ -1,5 +1,6 @@
 package pl.tkaras.models.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class AnswerDTO {
 
     private String questionId;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Integer correctAnswer;
 
     private boolean isAnswerRight;
