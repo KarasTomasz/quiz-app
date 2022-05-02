71829ef05ba2087e86df2947034787f2c1c15d95
KarasTomasz <t.karas@onet.pl>
2022-05-02 22:04:01 +0200

    add AppUserRole

---
+++ src/main/java/pl/tkaras/models/documents/AppUserRole.java
@@ -0,0 +1,16 @@
+package pl.tkaras.models.documents;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AppUserRole {
+    ROLE_USER, ROLE_ADMIN;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority(){
+        Set<SimpleGrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return grantedAuthorities;
+    }
+}
