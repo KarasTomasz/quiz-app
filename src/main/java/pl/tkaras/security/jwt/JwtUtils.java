c4c90eb8e05e1417b6ede00877dac8e0f66c7260
KarasTomasz <t.karas@onet.pl>
2022-05-02 22:05:59 +0200

    add JwtUtils

---
+++ src/main/java/pl/tkaras/security/jwt/JwtUtils.java
@@ -0,0 +1,42 @@
+package pl.tkaras.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Getter
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.tokenHeader}")
+    private String tokenHeader;
+
+    @Value("${jwt.tokenPrefix}")
+    private String tokenPrefix;
+
+    @Value("${jwt.jwtSecretKey}")
+    private String tokenSecretKey;
+
+    @Value("${jwt.expTime}")
+    private long expirationTime;
+
+
+    public String generateJwtToken(Authentication authResult) {
+
+        return Jwts.builder()
+                .setSubject(authResult.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.ES256, tokenSecretKey)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token){
+        return Jwts.parser().setSigningKey(tokenSecretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+}
