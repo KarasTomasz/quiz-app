47eb9953b30eb9e4d67f16485a7c8209f6044184
KarasTomasz <t.karas@onet.pl>
2022-05-02 22:02:14 +0200

    add SecurityConfig

---
+++ src/main/java/pl/tkaras/configs/SecurityConfig.java
@@ -0,0 +1,59 @@
+package pl.tkaras.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.tkaras.security.jwt.JwtAuthTokenFilter;
+import pl.tkaras.security.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import pl.tkaras.security.jwt.JwtUtils;
+import pl.tkaras.security.service.AppUserDetailsServiceImpl;
+
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtUtils jwtUtils;
+
+    private final AppUserDetailsServiceImpl appUserDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtUtils))
+                .addFilterAfter(new JwtAuthTokenFilter(jwtUtils, appUserDetailsService), JwtUsernameAndPasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/","/api/v1/appUser/auth/**", "/login")
+                    .permitAll()
+                .antMatchers("/swagger-ui.html", "/v2/api-docs", "/webjars/**", "/swagger-resources/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    private DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(appUserDetailsService);
+        return provider;
+    }
+}
