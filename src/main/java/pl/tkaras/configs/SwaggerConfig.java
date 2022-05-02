e58982b2cf316e903849bbfad54d9c066d478b62
KarasTomasz <t.karas@onet.pl>
2022-05-02 22:01:28 +0200

    add createSchema, createContext, createRef

--- src/main/java/pl/tkaras/configs/SwaggerConfig.java
+++ src/main/java/pl/tkaras/configs/SwaggerConfig.java
@@ -2,14 +2,18 @@ package pl.tkaras.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 @EnableSwagger2
 @Configuration
@@ -18,10 +22,13 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(UsernamePasswordAuthenticationToken.class)
                 .select()
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
-                .apiInfo(createApiInfo());
+                .apiInfo(createApiInfo())
+                .securitySchemes(singletonList(createSchema()))
+                .securityContexts(singletonList(createContext()));
     }
 
     private ApiInfo createApiInfo() {
@@ -35,4 +42,21 @@ public class SwaggerConfig {
                 Collections.emptyList());
     }
 
+    private SecurityScheme createSchema() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext createContext() {
+        return SecurityContext.builder()
+                .securityReferences(createRef())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> createRef(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
 }
