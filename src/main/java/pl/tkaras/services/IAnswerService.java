3b05febf2f42e28338b0a01505a321a764709edc
KarasTomasz <t.karas@onet.pl>
2022-05-04 12:46:16 +0200

    remove addAnswer, deleteAnswer; rename getAnswer

--- src/main/java/pl/tkaras/services/IAnswerService.java
+++ src/main/java/pl/tkaras/services/IAnswerService.java
@@ -4,13 +4,9 @@ import pl.tkaras.models.documents.Answer;
 
 public interface IAnswerService {
 
-    Answer getAnswer(String id);
+    Answer getAnswerByQuestionId(String questionID);
 
-    Answer addAnswer(Answer answer);
+    boolean checkAnswer(String email, String questionID, Integer num);
 
-    boolean checkAnswer(String email, String questionId, Integer num);
-
-    Answer updateAnswer(String id, Answer answerDTO);
-
-    void deleteAnswer(String id);
+    Answer updateAnswer(String questionID, Answer answerDTO);
 }
