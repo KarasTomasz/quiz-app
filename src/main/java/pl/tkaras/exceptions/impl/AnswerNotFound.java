85c12481cfadc835e626a7265c408299bab20384
KarasTomasz <t.karas@onet.pl>
2022-05-04 12:44:14 +0200

    add ResponseStatus annotation

--- src/main/java/pl/tkaras/exceptions/impl/AnswerNotFound.java
+++ src/main/java/pl/tkaras/exceptions/impl/AnswerNotFound.java
@@ -1,7 +1,10 @@
 package pl.tkaras.exceptions.impl;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.tkaras.exceptions.NotFoundException;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class AnswerNotFound extends NotFoundException {
 
     public AnswerNotFound(String className, String s) {
